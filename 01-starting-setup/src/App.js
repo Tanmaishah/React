50de27ab06dd630c97801a6b238f5b13a44203a2
Tanmaishah <65083113+Tanmaishah@users.noreply.github.com>
2023-03-06 23:11:10 +0530

    Movint to components part

---
+++ 01-starting-setup/src/App.js
@@ -0,0 +1,11 @@
+import Expenseitem from "./components/expenseitem";
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenseitem />// must always start with always Capital letter.
+    </div>
+  );
+}
+
+export default App;
---
+++ 01-starting-setup/src/components/expenseitem.js
@@ -0,0 +1,6 @@
+function expenseitem(){// we can give the function any name but its a convention to give the same name as the file name.
+return(
+    <h1>ExpenseItem</h1>
+)
+}
+export default expenseitem
